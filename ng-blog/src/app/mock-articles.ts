0ce0fbd7ad68a848d92b8f5e0061bc9a9bc60ec4
osidekyle <hawkinskyle67@gmail.com>
2021-03-09 00:51:53 -0800

    set up article list

---
+++ ng-blog/src/app/mock-articles.ts
@@ -0,0 +1,25 @@
+import {Article} from "./article"
+
+
+
+export const ARTICLES:Article[] =[
+    {
+        id:1,
+        title:"My first article",
+        content:'',
+        description:"This is my first article",
+        key:'my-first-article',
+        date:new Date(),
+        imageUrl:'http://angular.io/assets/images/logos/angular/angular.png'
+    },
+    {
+        id:2,
+        title:"My second article",
+        content:'',
+        description:"This is my second article",
+        key:'my-second-article',
+        date:new Date(),
+        imageUrl:'http://angular.io/assets/images/logos/angular/angular.png'
+    }
+];
+
